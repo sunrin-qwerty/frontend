569b0237423a3e598b3e199f95881ca6d72db566
kimdohyun <dohyun2008@kakao.com>
2024-11-25 11:30:27 +0900

    set css code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 import router from './router.jsx'
 import { RouterProvider } from 'react-router-dom'
 
